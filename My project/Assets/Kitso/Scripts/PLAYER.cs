7d473a205691f5fbb11c9616e582edfe1cf9a728
Blessingsoko <blessingsoko44@gmail.com>
2026-04-17 01:39:35 +0200

    updates

--- My project/Assets/Kitso/Scripts/PLAYER.cs
+++ My project/Assets/Kitso/Scripts/PLAYER.cs
@@ -1,21 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class PLAYER : MonoBehaviour
 {
-    public float speed;
+    public GameObject dialoguePanel;
+    public Text dialogueText;
+    public string[] dialogue;
+    private int index;
 
-    
-   
+    public float wordspeed;
+    public bool playerIsClose; 
 
-    // Update is called once per frame
+
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        if(Input.GetKeyDown(KeyCode.E) && playerIsClose)
         {
-            Debug.Log("Pressed the arrow key");
+
+            if (dialoguePanel.activeInHierarchy)
+            {
+                zeroText();
+            }
+            else
+            {
+                dialoguePanel.SetActive(true);
+                StartCoroutine(Typing());
+            }
 
 
+        }
+    }
+
+
+    public void zeroText()
+    {
+        dialogueText.text = "";
+        index = 0;
+        dialoguePanel.SetActive(false);
+    }
+
+    IEnumerator Typing()
+    {
+        foreach(char letter in dialogue[index].ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(wordspeed);
+        }
+    }
+
+    public void NextLine()
+    {
+        if(index  < dialogue.Length -1)
+        {
+            index++;
+            dialogueText.text = "";
+            StartCoroutine(Typing());
+        }
+        else
+        {
+            zeroText();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            playerIsClose = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerIsClose = false;
+            zeroText();
         }
     }
 }
