353ec00a3a92724e4f5ea812c78c75e35af383d0
kitsobusang4-glitch <kitsobusang4@gmail.com>
2026-04-17 12:10:58 +0200

    Fight Scene Remastered

---
+++ My project/Assets/Kitso/Scripts/BattleSystem.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST }
+
+public class BattleSystem : MonoBehaviour
+{
+    public GameObject playerPrefab;
+    public GameObject enemyPrefab;
+
+    public Transform playerBattleStation;
+    public Transform enemyBattleStation;
+
+    Unit playerUnit;
+    Unit enemyUnit;
+
+    public TextMeshProUGUI dialogueText;
+
+    public BattleHUD playerHUD;
+    public BattleHUD enemyHUD;
+
+    public BattleState state;
+   
+    void Start()
+    {
+        state = BattleState.START;
+        SetupBattle();
+    }
+
+    void SetupBattle()
+    {
+        GameObject playerGO = Instantiate(playerPrefab, playerBattleStation);
+        playerUnit = playerGO.GetComponent<Unit>();
+
+        GameObject enemyGO = Instantiate(enemyPrefab, enemyBattleStation);
+        enemyUnit = enemyGO.GetComponent<Unit>();
+
+        dialogueText.text = "A crazy " + enemyUnit.unitName + " wants to pick a fight";
+
+        playerHUD.SetHUD(playerUnit);
+        enemyHUD.SetHUD(enemyUnit);
+
+        state = BattleState.PLAYERTURN;
+        
+    }
+    
+    IEnumerator PlayerAttack()
+    {
+        // Damage the enemy
+        bool isDead = enemyUnit.TakeDamage(playerUnit.damage);
+
+        enemyHUD.SetHP(enemyUnit.currentHP);
+
+        yield return new WaitForSeconds(2f);
+
+        // Check if the enemy is dead
+        if (isDead)
+        {
+            state = BattleState.WON;
+           
+            //end the battle
+        } else
+        {
+            //enemy turn
+            state = BattleState.ENEMYTURN;
+            StartCoroutine(EnemyTurn());
+        }
+
+        // Change state based on what happened
+    }
+
+    IEnumerator EnemyTurn()
+    {
+        yield return new WaitForSeconds(1f);
+
+       bool isDead = playerUnit.TakeDamage(enemyUnit.damage);
+
+        playerHUD.SetHP(playerUnit.currentHP);
+
+        yield return new WaitForSeconds(1f);
+
+        if (isDead)
+        {
+            state = BattleState.LOST;
+            
+        } else
+        {
+            state = BattleState.PLAYERTURN;
+            
+        }
+    }
+    
+    public void OnAttackButton()
+    {
+        if (state != BattleState.PLAYERTURN)
+            return;
+
+        StartCoroutine(PlayerAttack());
+    }
+}
