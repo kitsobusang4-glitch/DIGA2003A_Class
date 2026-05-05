fd27766038f155b3debb21fbd2d640c25e026ded
kitsobusang4-glitch <kitsobusang4@gmail.com>
2026-05-05 19:38:45 +0200

    Reapply "Merge branch 'KITSO' of https://github.com/kitsobusang4-glitch/DIGA2003A_Class into KITSO"
    This reverts commit bffb1d5c00f08e86baf9fa6f4d49a54e75f12d04.

---
+++ My project/Assets/Blessing/Scripts (Blessing)/SaveData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic; 
+using UnityEngine;
+
+[System.Serializable]
+public class SaveData
+{
+    public Vector3 playerPosition;
+    public string mapBoundary; //The boundary name for the map
+    public List<InventorySaveData> InventorySaveData;
+}
